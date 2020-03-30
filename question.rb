53f96f7e858b05ba9f273a5f2a9ea2ea40a8598a
Rayesh <rayeshrajah@gmail.com>
2020-03-30 13:26:31 -0400

    added question class with questions and answers

--- question.rb
+++ question.rb
@@ -1,3 +1,9 @@
 class Question
-  
+  attr_accessor 
+  def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    question = "What does #{num1} plus #{num2} equal to?"
+    question_answer = num1 + num2
+  end
 end
