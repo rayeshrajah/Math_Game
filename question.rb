4a93cab4f058a78efb8ebbda0c5099cd4b010e0a
Rayesh <rayeshrajah@gmail.com>
2020-03-30 13:30:36 -0400

    Added check-answer method in question class

--- question.rb
+++ question.rb
@@ -1,9 +1,15 @@
 class Question
-  attr_accessor 
+  attr_accessor :num1, :num2, :question, :question_answer
   def initialize
-    num1 = rand(1..20)
-    num2 = rand(1..20)
-    question = "What does #{num1} plus #{num2} equal to?"
-    question_answer = num1 + num2
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @question = "What does #{num1} plus #{num2} equal to?"
+    @question_answer = num1 + num2
   end
+
+  def check_ans(ans)
+    #puts the user input as an interger
+    ans.to_i == @question_answer
+  end
+
 end
